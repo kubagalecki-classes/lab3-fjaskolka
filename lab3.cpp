0aa19ebc1f843c0a22b43cbc683aaa1104bb0986
fjaskolka <filipjask512@gmail.com>
2020-11-22 15:30:27 +0000

    Lab3 V1

--- lab3.cpp
+++ lab3.cpp
@@ -1,6 +1,40 @@
+#include <cmath>
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+class Figura
+{
+public:
+    Figura(double pole_p) : pole{pole_p}
+    {
+        void   set_p(double p) { pole = p; }
+        double get_p() { return pole; }
+    }
+
+private:
+    double pole;
+};
+
+class Kolo : public Figura
+{
+public:
+    Kolo(double r) : Figura{3.14 * r * r} { get_p(); }
+};
+
+// class Kwadrat : public Figura
+//{
+// public:
+//    Kwadrat(double a) : Figura{pole} { set_p(a * //a); }
+//};
 
 int main()
 {
-    puts("Hello, World!");
+    Figura a();
+    a.set_p(5);
+    cout << a.get_p() << endl << endl;
+    cout << Kolo ko1(67.45);
+    // cout << ko1.get_p() << endl;
+    // Kwadrat kw1(46.78);
+    // cout << kw1.get_p() << endl;
 }
--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,23 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int suma_z = 0;
+    unsigned int suma_t = 0;
+    Stocznia stocznia{};
+
+    while (suma_t <= towar)
+    {
+      if( towar == 0)
+      {
+        break;
+      }
+      Statek* statek = stocznia();
+      suma_t += statek -> transportuj();
+      if (dynamic_cast < Zaglowiec* > (statek))
+      {
+        suma_z ++
+      };
+      delete statek;
+    }
+    return suma_z;
 }
